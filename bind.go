3f6744a4623060257e4ca98e62f18839fabac776
Jianwei Li <jianwei.li@shopee.com>
2019-03-04 22:40:43 +0800

    add

--- bind.go
+++ bind.go
@@ -39,6 +39,7 @@ func (b *DefaultBinder) Bind(i interface{}, c Context) (err error) {
 		}
 		return NewHTTPError(http.StatusBadRequest, "Request body can't be empty")
 	}
+
 	ctype := req.Header.Get(HeaderContentType)
 	switch {
 	case strings.HasPrefix(ctype, MIMEApplicationJSON):
@@ -64,42 +65,73 @@ func (b *DefaultBinder) Bind(i interface{}, c Context) (err error) {
 		if err != nil {
 			return NewHTTPError(http.StatusBadRequest, err.Error()).SetInternal(err)
 		}
+
+		// core logic
 		if err = b.bindData(i, params, "form"); err != nil {
 			return NewHTTPError(http.StatusBadRequest, err.Error()).SetInternal(err)
 		}
+
 	default:
 		return ErrUnsupportedMediaType
 	}
 	return
 }
 
+
+
 func (b *DefaultBinder) bindData(ptr interface{}, data map[string][]string, tag string) error {
-	typ := reflect.TypeOf(ptr).Elem()
+
+	// 获取指针变量的反射对象时，可以通过 reflect.Elem() 方法获取这个指针指向的元素类型。
+	// 这个获取过程被称为取元素，等效于对指针类型变量做了一个*操作。
+
+
+
+	typ := reflect.TypeOf(ptr).Elem()  	//变量类型
+
+
+	// reflect.Value 是通过 reflect.ValueOf(x) 获得的，只有当X是指针的时候，才可以通过reflec.Value修改实际变量 x 的值。
+	// 由于传入的是指针，需要用 p.Elem() 获取所指向的 v；v.CantSet()输出的是true时便可以用 v.SetFloat() 修改 x 的值。
 	val := reflect.ValueOf(ptr).Elem()
 
+
+	// Kind() 返回 rtype.kind，描述一种基础类型
 	if typ.Kind() != reflect.Struct {
 		return errors.New("binding element must be a struct")
 	}
 
+
 	for i := 0; i < typ.NumField(); i++ {
+
+		// reflect.StructField: 反射获取结构体字段的元信息，例如：字段名称、Tags 等
 		typeField := typ.Field(i)
-		structField := val.Field(i)
+
+		// reflect.value: 反射获取&修改字段值
+		structField := val.Field(i) //字段值
 		if !structField.CanSet() {
 			continue
 		}
-		structFieldKind := structField.Kind()
-		inputFieldName := typeField.Tag.Get(tag)
 
+		// 获取 reflect.value 的基础类型（非定义的静态类型）
+		structFieldKind := structField.Kind()
+
+		// 获取 reflect.StructField 字段的指定 tag
+		inputFieldName := typeField.Tag.Get(tag)
 		if inputFieldName == "" {
+			// 如果tag为空，就用字段名来表示
 			inputFieldName = typeField.Name
-			// If tag is nil, we inspect if the field is a struct.
-			if _, ok := bindUnmarshaler(structField); !ok && structFieldKind == reflect.Struct {
-				if err := b.bindData(structField.Addr().Interface(), data, tag); err != nil {
-					return err
+			// 如果tag为空，检查该字段是否是嵌套结构体
+			if _, ok := bindUnmarshaler(structField); !ok {
+				//判断是否是嵌套结构体
+				if structFieldKind == reflect.Struct { 
+					//递归调用，
+					if err := b.bindData(structField.Addr().Interface(), data, tag); err != nil {
+						return err
+					}
+					continue
 				}
-				continue
 			}
 		}
+
 
 		inputValue, exists := data[inputFieldName]
 		if !exists {
@@ -121,6 +153,7 @@ func (b *DefaultBinder) bindData(ptr interface{}, data map[string][]string, tag 
 			continue
 		}
 
+
 		// Call this first, in case we're dealing with an alias to an array type
 		if ok, err := unmarshalField(typeField.Type.Kind(), inputValue[0], structField); ok {
 			if err != nil {
@@ -131,31 +164,53 @@ func (b *DefaultBinder) bindData(ptr interface{}, data map[string][]string, tag 
 
 		numElems := len(inputValue)
 		if structFieldKind == reflect.Slice && numElems > 0 {
+			//切片元素类型
 			sliceOf := structField.Type().Elem().Kind()
+			//创建切片
 			slice := reflect.MakeSlice(structField.Type(), numElems, numElems)
+			//切片逐元素赋值
 			for j := 0; j < numElems; j++ {
 				if err := setWithProperType(sliceOf, inputValue[j], slice.Index(j)); err != nil {
 					return err
 				}
 			}
+			//变量赋值
 			val.Field(i).Set(slice)
+		} else if _, isTime := structField.Interface().(time.Time); isTime {
+			return setTimeField(inputValue, *typeField, *structField)
 		} else if err := setWithProperType(typeField.Type.Kind(), inputValue[0], structField); err != nil {
 			return err
-
 		}
 	}
 	return nil
 }
 
+
+
+
+
+
+
+//获取 reflect.Kind 对应的golang基础类型关系，以便进行类型转换
+
 func setWithProperType(valueKind reflect.Kind, val string, structField reflect.Value) error {
+	
+
 	// But also call it here, in case we're dealing with an array of BindUnmarshalers
 	if ok, err := unmarshalField(valueKind, val, structField); ok {
 		return err
 	}
 
+
 	switch valueKind {
 	case reflect.Ptr:
+		//判断 v 是否合法，如果返回 false，那么除了 String() 以外的其他方法调用都会 panic，事前检查是必要的
+		if !structField.Elem().IsValid() {
+			structField.Set(reflect.New(structField.Type().Elem()))
+		}
+		//如果值是指针类型，则获取其指向的值类型structField.Elem().Kind()和值对象structField.Elem()，然后执行写入val
 		return setWithProperType(structField.Elem().Kind(), val, structField.Elem())
+
 	case reflect.Int:
 		return setIntField(val, 0, structField)
 	case reflect.Int8:
@@ -190,6 +245,9 @@ func setWithProperType(valueKind reflect.Kind, val string, structField reflect.V
 	return nil
 }
 
+
+
+
 func unmarshalField(valueKind reflect.Kind, val string, field reflect.Value) (bool, error) {
 	switch valueKind {
 	case reflect.Ptr:
@@ -201,15 +259,26 @@ func unmarshalField(valueKind reflect.Kind, val string, field reflect.Value) (bo
 
 // bindUnmarshaler attempts to unmarshal a reflect.Value into a BindUnmarshaler
 func bindUnmarshaler(field reflect.Value) (BindUnmarshaler, bool) {
+
+
+
+			
 	ptr := reflect.New(field.Type())
+	//用CanInterface()来判断是否为可导出字段，读取未导出变量时会panic，
+	//panic: reflect.Value.Interface: cannot return value obtained from unexported field or method
 	if ptr.CanInterface() {
+		// 执行转换，转成interface
 		iface := ptr.Interface()
 		if unmarshaler, ok := iface.(BindUnmarshaler); ok {
 			return unmarshaler, ok
 		}
 	}
+
+	//不可导出
 	return nil, false
 }
+
+
 
 func unmarshalFieldNonPtr(value string, field reflect.Value) (bool, error) {
 	if unmarshaler, ok := bindUnmarshaler(field); ok {
@@ -220,6 +289,7 @@ func unmarshalFieldNonPtr(value string, field reflect.Value) (bool, error) {
 	return false, nil
 }
 
+
 func unmarshalFieldPtr(value string, field reflect.Value) (bool, error) {
 	if field.IsNil() {
 		// Initialize the pointer to a nil value
@@ -227,6 +297,8 @@ func unmarshalFieldPtr(value string, field reflect.Value) (bool, error) {
 	}
 	return unmarshalFieldNonPtr(value, field.Elem())
 }
+
+
 
 func setIntField(value string, bitSize int, field reflect.Value) error {
 	if value == "" {
@@ -271,3 +343,52 @@ func setFloatField(value string, bitSize int, field reflect.Value) error {
 	}
 	return err
 }
+
+func setTimeField(value string, structField reflect.StructField, field reflect.Value) error {
+	timeFormat := structField.Tag.Get("time_format")
+	if timeFormat == "" {
+		return errors.New("Blank time format")
+	}
+
+	if value == "" {
+		field.Set(reflect.ValueOf(time.Time{}))
+		return nil
+	}
+
+
+	l := time.Local
+	if isUTC, _ := strconv.ParseBool(structField.Tag.Get("time_utc")); isUTC {
+		l = time.UTC
+	}
+
+	if locTag := structField.Tag.Get("time_location"); locTag != "" {
+		loc, err := time.LoadLocation(locTag)
+		if err != nil {
+			return err
+		}
+		l = loc
+	}
+
+	t, err := time.ParseInLocation(timeFormat, value, l)
+	if err != nil {
+		return err
+	}
+
+	field.Set(reflect.ValueOf(t))
+	return nil
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
